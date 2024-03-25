ac71b04659d40c64057c4be088d51c2cc8e1ac38
apipoy <piipps.f@gmail.com>
2024-03-26 00:04:10 +0700

    signup & cart

--- features/authFeatures/src/main/java/com/nabiha/authfeatures/register/RegisterScreen.kt
+++ features/authFeatures/src/main/java/com/nabiha/authfeatures/register/RegisterScreen.kt
@@ -1,14 +1,17 @@
 package com.nabiha.authfeatures.register
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -19,15 +22,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.nabiha.apiresponse.users.UserApiRegisterRequest
+import com.nabiha.authfeatures.R
 import com.nabiha.designsystem.theme.OptixTheme
 import kotlinx.coroutines.launch
 
@@ -48,6 +56,10 @@ private fun RegisterScreen(
 ) {
 
 
+    var userName by remember {
+        mutableStateOf("")
+    }
+
     var email by remember {
         mutableStateOf("")
     }
@@ -56,16 +68,12 @@ private fun RegisterScreen(
         mutableStateOf("")
     }
 
-    var name by remember {
-        mutableStateOf("")
-    }
-
-    var phone by remember {
+    var confirmPassword by remember {
         mutableStateOf("")
     }
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().background(color=MaterialTheme.colorScheme.background)
     ) {
         item {
             Box(
@@ -76,12 +84,38 @@ private fun RegisterScreen(
             ) {
                 Column (modifier = Modifier.fillMaxSize()) {
                     OutlinedTextField(
+                        value = userName,
+                        onValueChange = { userName = it },
+                        textStyle = MaterialTheme.typography.bodyLarge,
+                        placeholder = { Text(text = "Username") },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.person_icn),
+                                contentDescription = "Username Icon",
+                                modifier = Modifier.padding(8.dp).size(24.dp)
+                            )
+                        },
+                        shape = RoundedCornerShape(20.dp),
+                        modifier = Modifier
+                            .padding(bottom = 12.dp)
+                            .fillMaxWidth(),
+                    )
+                    OutlinedTextField(
                         value = email,
                         onValueChange = { email = it },
                         textStyle = MaterialTheme.typography.bodyLarge,
                         placeholder = { Text(text = "Email") },
                         singleLine = true,
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.mail_icn),
+                                contentDescription = "Username Icon",
+                                modifier = Modifier.padding(8.dp).size(24.dp)
+                            )
+                        },
                         shape = RoundedCornerShape(20.dp),
                         modifier = Modifier
                             .padding(bottom = 12.dp)
@@ -94,30 +128,34 @@ private fun RegisterScreen(
                         placeholder = { Text(text = "Password") },
                         singleLine = true,
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.lock_icn),
+                                contentDescription = "Username Icon",
+                                modifier = Modifier.padding(8.dp).size(24.dp)
+                            )
+                        },
+                        visualTransformation = PasswordVisualTransformation(),
                         shape = RoundedCornerShape(20.dp),
                         modifier = Modifier
                             .padding(bottom = 12.dp)
                             .fillMaxWidth(),
                     )
                     OutlinedTextField(
-                        value = name,
-                        onValueChange = { name = it },
+                        value = confirmPassword,
+                        onValueChange = { confirmPassword = it },
                         textStyle = MaterialTheme.typography.bodyLarge,
-                        placeholder = { Text(text = "Name") },
+                        placeholder = { Text(text = "Confirm Password") },
                         singleLine = true,
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                        shape = RoundedCornerShape(20.dp),
-                        modifier = Modifier
-                            .padding(bottom = 12.dp)
-                            .fillMaxWidth(),
-                    )
-                    OutlinedTextField(
-                        value = phone,
-                        onValueChange = { phone = it },
-                        textStyle = MaterialTheme.typography.bodyLarge,
-                        placeholder = { Text(text = "Phone") },
-                        singleLine = true,
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.lock_icn),
+                                contentDescription = "Username Icon",
+                                modifier = Modifier.padding(8.dp).size(24.dp)
+                            )
+                        },
+                        visualTransformation = PasswordVisualTransformation(),
                         shape = RoundedCornerShape(20.dp),
                         modifier = Modifier
                             .padding(bottom = 12.dp)
@@ -138,7 +176,7 @@ private fun RegisterScreen(
                      //}
 
                     }) {
-                        Text(text = "Register User")
+                        Text(text = "Sign Up")
                     }
                 }
 
